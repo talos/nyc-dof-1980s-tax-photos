6a8a9c2df400b28f1a2241747912a41314825c9a
John Krauss <irving.krauss@gmail.com>
2015-01-12 16:53:36 -0500

    work for all boroughs

--- download.sh
+++ download.sh
@@ -3,24 +3,41 @@
 IFS="
 "
 mkdir -p output
-x=0
-while [ $x -lt 500000 ]
+
+BRONX='http://nycma.lunaimaging.com/luna/servlet/RECORDSPHOTOUNITBRO~4~4'
+BROOKLYN='http://nycma.lunaimaging.com/luna/servlet/RECORDSPHOTOUNITBRK~1~1'
+MANHATTAN='http://nycma.lunaimaging.com/luna/servlet/RECORDSPHOTOUNITMAN~2~2'
+QUEENS='http://nycma.lunaimaging.com/luna/servlet/RECORDSPHOTOUNITQUE~1~1'
+STATEN_ISLAND='http://nycma.lunaimaging.com/luna/servlet/RECORDSPHOTOUNITSTA~1~1'
+
+COOKIEJAR=cookiejar
+
+for borough in $BRONX $BROOKLYN $MANHATTAN $QUEENS $STATEN_ISLAND
 do
-  url="http://nycma.lunaimaging.com/luna/servlet/view/all?pgs=250&os=$x"
-  echo $url
-  for line in $(curl -s --compressed --cookie 'JSESSIONID=762E4631B42A71B1ED76FA226CEEDE3E' \
-    $url | \
-    grep -Po 'alt="dof_[^"]+" src="http://nycma.lunaimaging.com/MediaManager/srvr\?mediafile=[^&]+')
+  # Browsing a specific collection isn't stored in URL, but in session cookie
+  # (!). To look at a collection, go to its landing page and store the cookie.
+  curl -s --compressed --cookie-jar $COOKIEJAR $borough > /dev/null
+  x=0
+  cat $COOKIEJAR
+  while [ $x -lt 500000 ]
   do
-    name=$(echo $line | grep -Po 'dof_[^"]+')
-    url=$(echo $line | grep -Po 'http://.*' | sed -e 's/Size1/Size3/')
-    if [ ! -e output/$name.jpg ]
-    then
-      echo $name
-      curl -s "$url" > output/$name.jpg
-      sleep 0.1
-    fi
+    url="http://nycma.lunaimaging.com/luna/servlet/view/all?pgs=250&os=$x"
+    echo $url
+    for line in $(curl --cookie $COOKIEJAR -s --compressed $url | \
+      grep -Po 'alt="dof_[^"]+" src="http://nycma.lunaimaging.com/MediaManager/srvr\?mediafile=[^&]+')
+    do
+      name=$(echo $line | grep -Po 'dof_[^"]+')
+      url=$(echo $line | grep -Po 'http://.*' | sed -e 's/Size1/Size3/')
+      if [ ! -e "output/$name.jpg" ]
+      then
+        echo $name
+        curl -s "$url" > output/$name.jpg
+        sleep 0.1
+      else
+        echo skipped $name
+      fi
+    done
+    x=$(( x + 250 ))
+    echo "x: $x"
   done
-  x=$(( x + 250 ))
-  echo "x: $x"
 done
